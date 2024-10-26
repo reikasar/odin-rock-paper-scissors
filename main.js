becd91d0d10e3193d383135a9435318803611885
reikasar <183247743+reikasar@users.noreply.github.com>
2024-10-26 18:55:48 +0300

    add playGame logic but doesnt call playRound function

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ console.log("Hey, Friend! Let's play a game of 'Rock-Paper-Scissors'\!")
 
 function getComputerChoice() {
   let genNumberOneToThree = Math.floor(Math.random()*3 + 1)
-/*  console.log(genNumberOneToThree) */
   if (genNumberOneToThree === 1) {
     return "rock";
   } else if (genNumberOneToThree === 2) {
@@ -12,51 +11,57 @@ function getComputerChoice() {
   }
 }
 
-/* console.log(getComputerChoice()) */
-
 function getHumanChoice() {
   let choice = prompt("Please enter you choice, good Sir!")
   lowerCaseChoice = choice.toLowerCase()
   return lowerCaseChoice
 }
 
-/* console.log(getHumanChoice()) */
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-  // your code here!
-  if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! Paper beats rock!");
-    computerScore = ++computerScore;
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats scissors!")
-    humanScore = ++humanScore;
-  }  else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats rock!")
-    humanScore = ++humanScore;
-  }  else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose! Scissors beat paper!")
-    computerScore = ++computerScore;
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose! Rock beats scissors!")
-    computerScore = ++computerScore;
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win! Scissors beats paper!")
-    humanScore = ++humanScore;
-  } else if ((humanChoice === "scissors" && computerChoice === "scissors") || (humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper")) {
-    console.log("It is a tie!")
-  }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-function playGame(playRound()) {
+function playGame() {
   let humanScore = 0;
   let computerScore = 0;
-  
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats rock!");
+      computerScore = ++computerScore;
+      return computerScore;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats scissors!")
+      humanScore = ++humanScore;
+      return humanScore;
+    }  else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats rock!")
+      humanScore = ++humanScore;
+      return humanScore;
+    }  else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beat paper!")
+      computerScore = ++computerScore;
+      return computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats scissors!")
+      computerScore = ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beats paper!")
+      humanScore = ++humanScore;
+      return computerScore;
+    } else if ((humanChoice === "scissors" && computerChoice === "scissors") || (humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper")) {
+      console.log("It is a tie!")
+    }
+  }
+  function playGameUntil() {
+  if (humanScore < 5 || computerScore < 5) {
+    playRound(humanChoice, computerChoice);
+  }  else if (computerScore == 5) {
+    console.log("Sadly you lost but let's play again!", humanScore, computerScore)
+  } else {
+    console.log("Gongratulations! You are the Victor!",humanScore, computerScore)
+  }
+}
+  playGameUntil();
 }
 
-
+playGame();
 
