beb34048f8c9b9b597fe766d807707583ee199f7
reikasar <183247743+reikasar@users.noreply.github.com>
2024-10-26 14:38:06 +0300

    add playrounf logic

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ console.log(getComputerChoice())
 
 //GET user input as prompt and store the value as getHumanChoice.
 function getHumanChoice() {
-  let humanChoice = prompt("Please enter you choice, good Sir!")
-  humanChoice = humanChoice.toLowerCase()
-  return humanChoice
+  let input = prompt("Please enter you choice, good Sir!")
+  input = input.toLowerCase()
+  return input
 }
 
 console.log(getHumanChoice())
@@ -30,7 +30,31 @@ let humanScore = 0
 
 let computerScore = 0
 
-//func 
 function playRound(humanChoice, computerChoice) {
-  switch 
+  // your code here!
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats rock!")
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats scissors!")
+  }  else if (humanChoice === "rock" && computerChoice === "rock") {
+      console.log("It is a tie!")
+  }  else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock!")
+  }  else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beat paper!")
+  }  else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("It is a tie!")
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats scissors!")
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats paper!")
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    console.log("It is a tie!")
+  }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
